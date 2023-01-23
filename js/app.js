8f2f911f610d4da463a5a724c3a35957a6c26531
Peace Hegemony <65087331+Tornacyclo@users.noreply.github.com>
2023-01-23 19:02:39 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
 const loader = document.querySelector('.loader');
-const loaderbook = document.querySelector('.loader');
+//const loaderbook = document.querySelector('.loaderbook');
 
+
+//Smoothly make the loader disappear and change its z-index to something lower than the buttons
 window.addEventListener('load', () => {
 
-    loader.classList.add('fondu-out');
-    loaderbook.classList.add('fondu-out');
-    document.body.classList.remove('no-scroll');
+  loader.classList.add('fondu-out');
+  //loaderbook.classList.add('fondu-out');
+  document.body.classList.remove('no-scroll');
+  loader.style.zIndex = "-1";
 
 })
 
+//Remove the elements that is not required anymore
 /* setTimeout(() => {
 
-  loader.classList.add('fondu-out');
-  loaderbook.classList.add('fondu-out');
-  document.body.classList.remove('no-scroll');
+  loader.style.display = "none";
+  loaderbook.style.display = "none";
 
 }, 5000) */
