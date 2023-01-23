4eafe0156151e1302b0d74a6c855fb8ea11d3ee2
Peace Hegemony <65087331+Tornacyclo@users.noreply.github.com>
2023-01-23 18:22:13 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 const loader = document.querySelector('.loader');
+const loaderbook = document.querySelector('.loader');
 
 window.addEventListener('load', () => {
 
     loader.classList.add('fondu-out');
+    loaderbook.classList.add('fondu-out');
+    document.body.classList.remove('no-scroll');
 
 })
+
+/* setTimeout(() => {
+
+  loader.classList.add('fondu-out');
+  loaderbook.classList.add('fondu-out');
+  document.body.classList.remove('no-scroll');
+
+}, 5000) */
