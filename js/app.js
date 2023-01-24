5aa3365f64a4e186cc8f90c13d7878bd84da42ba
Peace Hegemony <65087331+Tornacyclo@users.noreply.github.com>
2023-01-24 02:47:50 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -23,4 +23,27 @@ setTimeout(() => {
   loader.style.display = "none";
   loaderbook.style.display = "none";
 
-}, 5000) */
+}, 5000)
+
+const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+
+const validate = () => {
+  const $result = $('#result');
+  const email = $('#emailAddress').val();
+  $result.text('');
+
+  if (validateEmail(email)) {
+    $result.text(email + ' is valid :)');
+    $result.css('color', 'green');
+  } else {
+    $result.text(email + ' is not valid :(');
+    $result.css('color', 'red');
+  }
+  return false;
+}
+
+$('#emailAddress').on('input', validate); */
