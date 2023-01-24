85c7e7872d3a2db2a9ad16cb7db454d009de1451
Peace Hegemony <65087331+Tornacyclo@users.noreply.github.com>
2023-01-24 04:14:16 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -14,7 +23,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
-    // Shrink the navbar
+    // Shrink the navbar 
     navbarShrink();
 
     // Shrink the navbar when page is scrolled
